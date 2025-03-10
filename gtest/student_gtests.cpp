4536eabce79833e1038b3a11b2dd1a195df59f5c
Landon94 <lkarkut@uci.edu>
2025-03-10 19:58:36 +0000

    Added tests for ladder.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,30 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+
+TEST(edit_distance_within, dif) {
+  EXPECT_TRUE(edit_distance_within("cat", "bat", 1));
+
+  EXPECT_TRUE(edit_distance_within("chat", "cheat", 1));
+
+  EXPECT_FALSE(edit_distance_within("chat", "fill", 1));
+}
+
+TEST(ladder, order) {
+  set<string> s;
+
+  load_words(s, "src/words.txt");
+
+  vector<string> ladder = generate_word_ladder("car", "cheat",s);
+
+  vector<string> expected = {
+    "car",
+    "cat",
+    "chat",
+    "cheat"
+  };
+
+  for (int i = 0; i < expected.size(); ++i) {
+    EXPECT_EQ(expected[i], ladder[i]);
+  }
 }
